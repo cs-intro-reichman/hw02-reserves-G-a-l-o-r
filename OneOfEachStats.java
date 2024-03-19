f4beb2864c056b589dcae76407c1f2e78738ede1
Gal <154674381+G-a-l-o-r@users.noreply.github.com>
2024-03-19 21:24:43 +0200

    done

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -68,7 +68,7 @@ public class OneOfEachStats {
 		System.out.println("Average: "+ avreage +" children to get at least one of each gender.");
 		System.out.println("Number of families with 2 children: "+ counter2);
 		System.out.println("Number of families with 3 children: "+ counter3);
-		System.out.println("Number of families with 4 children: "+ counter4);
+		System.out.println("Number of families with 4 or more children: "+ counter4);
 		System.out.println(most);
 
 		
